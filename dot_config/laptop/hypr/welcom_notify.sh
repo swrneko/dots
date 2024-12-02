32d9403b8749670b254c31ceb0e20da59cd89090
swrneko <sawarinekodesu@gmail.com>
2024-12-03 00:12:54 +0300

    commit

--- dot_config/laptop/hypr/welcom_notify.sh
+++ dot_config/laptop/hypr/welcom_notify.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-mako -c /home/swrneko/.config/mako/config_welcome
-notify-send おかえりなさい swrneko!
-killall -9 mako
-mako -c /home/swrneko/.config/mako/config
-
+makoctl mode -a welcom
+sleep 2.4
+notify-send  "おかえりなさい、swrneko!"  "(づ｡◕‿‿◕｡)づ❤" 
+makoctl mode -t welcom
