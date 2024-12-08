e5f800719de97b43e1b9dff7ef10822306e6dd7c
swrneko <sawarinekodesu@gmail.com>
2024-12-08 20:46:28 +0300

    commit

--- dot_config/pc/hypr/scripts/decorations.sh
+++ dot_config/pc/hypr/scripts/decorations.sh
@@ -4,17 +4,20 @@
 var_stacking_mode=~/.config/hypr/scripts/var/staking_mode_state.txt # Переменная состояния bar'ов 
 stacking_mode_is_toggle="`cat $var_stacking_mode`" # Включён ли staking_mode
 
-# При аргументе --bars включение/выключение плагина hyprland hyprbars в зависимости от состояния плагина
-if [[ "$@" =~ "--bars" ]];
+
+# При аргументе --stacking-mode включение/выключение плагина hyprland hyprbars в зависимости от состояния плагина и добавление параметров в конфиг hyprland
+if [[ "$@" =~ "--stacking_mode" ]];
 then
 
   if [ "$stacking_mode_is_toggle" == "false" ];
   then
-    echo "true" > "$var_stacking_mode"
-    hyprpm enable hyprbars -nn && hyprctl dismissnotify   
+    echo "true" > "$var_stacking_mode" # Изменение состояния stacking mode - включено
+    sed -i '232i\windowrulev2 = float, class:.*' ~/.config/hypr/hyprland.conf # Добавление на 232 строку конфига hyprland режима floating по умолчанию
+    hyprpm enable hyprbars -nn && hyprctl dismissnotify # Включение плагина hyprbars и отклонение уведомления о включении
   else
-    echo "false" > "$var_stacking_mode"
-    hyprpm disable hyprbars && hyprctl dismissnotify 
+    echo "false" > "$var_stacking_mode" # Изменение состояния stacking mode - выключено
+    sed -i '/windowrulev2 = float, class:.*/d' ~/.config/hypr/hyprland.conf # Удаление 232 строки конфига hyprland режима floating по умолчанию
+    hyprpm disable hyprbars && hyprctl dismissnotify  # Отключение плагина hyprbars и отклонение уведомления об отключении
   fi
 
 fi
@@ -41,4 +44,5 @@ then
   echo -e "--help\n    Show this message"
   echo -e "--snow\n    Meke snow on wallapepr"
   echo -e "--startup-notify\n    Exec startup notify"
+  echo -e "--stacking-mode\n    Toggle stacking mode"
 fi
